5b42d765e0e08c66711c05190ccbb5b079158854
Anton Kopanev <kopanewanton@gmail.com>
2021-11-21 18:04:05 +0200

    Completed countFiles method

--- src/main/java/com/luxoft/IO/FileManager.java
+++ src/main/java/com/luxoft/IO/FileManager.java
@@ -1,4 +1,21 @@
 package com.luxoft.IO;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class FileManager {
+  public static int countFiles(String path){
+    File curPath = new File(path);;
+    int result = 0;
+    if(Objects.requireNonNull(curPath.listFiles()).length == 0) return result;
+
+    for(File curFile: Objects.requireNonNull(curPath.listFiles())){
+      if(curFile.isDirectory())
+        result += countFiles(curFile.getPath());
+      else result++;
+    }
+
+    return result;
+  }
 }
--- src/test/java/com/luxoft/IO/FileManagerTest.java
+++ src/test/java/com/luxoft/IO/FileManagerTest.java
@@ -1,4 +1,12 @@
 package com.luxoft.IO;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FileManagerTest {
+  @Test
+  public void testCountFiles(){
+    int actual = FileManager.countFiles("src/test/resources");
+    assertEquals(7, actual);
+  }
 }
